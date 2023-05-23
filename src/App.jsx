39ca5a6e8c55ca53b48cea48d0dae547d5883f45
홍다미 <hongdami@hongdamiui-MacBookPro.local>
2023-05-24 08:00:28 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="text-3xl font-bold underline bg-purple-300 flex min-h-screen justify-center items-center">
+      <div className="bg-red-100 w-20 h-20">box</div>
+      <div className="bg-red-100 w-20 h-20">box</div>
+      <div className="bg-red-100 w-20 h-20">box</div>
+      <div className="bg-red-100 w-20 h-20">box</div>
+      <div className="bg-red-100 w-20 h-20">box</div>
+      <div className="bg-red-100 w-20 h-20">box</div>
+      <div className="bg-red-100 w-20 h-20">box</div>
+    </div>
+  );
+}
+
+export default App;
